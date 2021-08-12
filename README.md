096a14b455f227bdb7c0acc559523ee35b220685
bilalkubilay-git <65001711+bilalkubilay-git@users.noreply.github.com>
2021-08-12 22:43:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_starter_kit
+ Flutter Starter Kit for new flutter Project.
---
+++ android/app/src/main/kotlin/com/example/flutter_starter_kit/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_starter_kit
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
